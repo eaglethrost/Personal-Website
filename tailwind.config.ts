f718b7e4f22a9f80a724b89bbf0cd83a58446ab0
eaglethrost <dimazanugrah12@gmail.com>
2025-09-27 21:14:08 +1000

    feat(infra): add root level files to biome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,84 +1,84 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			screens: {
-				"tall-screen": { raw: "(min-height: 1000px)" },
-			},
-			colors: {
-				background: "var(--background)",
-				foreground: "var(--foreground)",
-			},
-			height: {
-				section: "832px",
-			},
-			minHeight: {
-				section: "100vh",
-			},
-			fontSize: {
-				header: "60px",
-			},
-			padding: {
-				start: "2rem",
-			},
-			keyframes: {
-				"fade-in-down": {
-					"0%": {
-						opacity: "0",
-						transform: "translateY(-50%)",
-					},
-					"100%": {
-						opacity: "1",
-						transform: "translateY(0)",
-					},
-				},
-				"fade-in-left": {
-					"0%": {
-						opacity: "0",
-						transform: "translateX(-15%)",
-					},
-					"100%": {
-						opacity: "1",
-						transform: "translateX(0)",
-					},
-				},
-				"fade-in-right": {
-					"0%": {
-						opacity: "0",
-						transform: "translateX(20%)",
-					},
-					"100%": {
-						opacity: "1",
-						transform: "translateX(0)",
-					},
-				},
-				"fade-in-up": {
-					"0%": {
-						opacity: "0",
-						transform: "translateY(20%)",
-					},
-					"100%": {
-						opacity: "1",
-						transform: "translateY(0)",
-					},
-				},
-			},
-			animation: {
-				fadeInDown: "fade-in-down 0.6s ease-in 0s 1",
-				fadeInDown2: "fade-in-down 0.6s ease-in 0.6s 1",
-				fadeInDown3: "fade-in-down 0.6s ease-in 1.2s 1",
-				fadeInLeft: "fade-in-left 0.8s ease-in 0s 1",
-				fadeInRight: "fade-in-right 0.8s ease-in 0s 1",
-				fadeInUp: "fade-in-up 0.8s ease-in 0s 1",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "tall-screen": { raw: "(min-height: 1000px)" },
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      height: {
+        section: "832px",
+      },
+      minHeight: {
+        section: "100vh",
+      },
+      fontSize: {
+        header: "60px",
+      },
+      padding: {
+        start: "2rem",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-50%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-left": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-15%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        "fade-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(20%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        fadeInDown: "fade-in-down 0.6s ease-in 0s 1",
+        fadeInDown2: "fade-in-down 0.6s ease-in 0.6s 1",
+        fadeInDown3: "fade-in-down 0.6s ease-in 1.2s 1",
+        fadeInLeft: "fade-in-left 0.8s ease-in 0s 1",
+        fadeInRight: "fade-in-right 0.8s ease-in 0s 1",
+        fadeInUp: "fade-in-up 0.8s ease-in 0s 1",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
